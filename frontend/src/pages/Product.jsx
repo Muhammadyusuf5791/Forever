d2ec4dfd68763ccbc4ab392ea0a158d9334882b4
muhammadyusuf5791 <muhammadyusufabdukarimov313@gmail.com>
2025-04-14 23:14:50 +0500

    newProject

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,11 +2,12 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
 
   const {productId} = useParams();
-  const {products, currency} = useContext(ShopContext);
+  const {products, currency, addToCart} = useContext(ShopContext);
   const [productData,setProductData] = useState(false);
   const [image,setImage] = useState('')
   const [size, setSize] = useState('')
@@ -66,7 +67,7 @@ const Product = () => {
               ))}
             </div>
           </div>
-          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
           <hr className='mt-8 sm:w-4/5' />
           <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1">
             <p>100% Original product.</p>
@@ -87,6 +88,9 @@ const Product = () => {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem voluptate minima illum nam numquam fuga reiciendis quis. A quaerat quod nostrum unde officia vel excepturi, ut sunt vero nihil ratione.</p>
         </div>
       </div>
+
+      {/* display related products */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
     </div>
   ) : <div className='opacity-0'></div>
 }
